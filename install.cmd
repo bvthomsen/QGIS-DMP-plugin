2f5395558b27c94dade161d22c441368c4dfd58e
Bo Victor Thomsen <bvt@aestas.dk>
2020-12-08 11:37:28 +0100

    Create initial build of plugin

---
+++ install.cmd
@@ -0,0 +1,5 @@
+@echo on
+set idir=dmp_manager
+rmdir /s /q "%appdata%\QGIS\QGIS3\profiles\default\python\plugins\%idir%"
+call .\%idir%\pyrcc5x.cmd
+xcopy .\%idir%\* "%appdata%\QGIS\QGIS3\profiles\default\python\plugins\%idir%" /i /e /h /f 
